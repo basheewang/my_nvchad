80c2f3253fbf43fba1e46a57310445b6ce3c095c
Coeus <coeus@mydebian.com>
2025-02-27 16:57:45 +0800

    Add Snacks plugin.

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,9 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- Snacks
+-- map("n", "<leader>Sp", ":lua Snacks.picker() <CR>", { desc = "Open Snacks Picker" })
+-- map("n", "<leader>Sf", ":lua Snacks.picker.files() <CR>", { desc = "Open Snacks to find file" })
+map("n", "<leader>e", ":lua Snacks.explorer() <CR>", { desc = "File explorer" })
+
