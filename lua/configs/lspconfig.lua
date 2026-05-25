9ab0867b43dd32685c5d16455b21c36fccdd7947
Coeus <bashee.wang@gmail.com>
2026-05-25 13:00:58 +0800

    fix errors in lspconfig file.

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,7 +1,7 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
+-- local lspconfig = require "lspconfig"
 
 -- EXAMPLE
 local servers = {
@@ -20,16 +20,18 @@ local servers = {
   "marksman",
   "bashls",
 }
-local nvlsp = require "nvchad.configs.lspconfig"
+vim.lsp.enable(servers)
 
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
-end
+-- local nvlsp = require "nvchad.configs.lspconfig"
+--
+-- -- lsps with default config
+-- for _, lsp in ipairs(servers) do
+--   lspconfig[lsp].setup {
+--     on_attach = nvlsp.on_attach,
+--     on_init = nvlsp.on_init,
+--     capabilities = nvlsp.capabilities,
+--   }
+-- end
 
 -- configuring single server, example: typescript
 -- lspconfig.ts_ls.setup {
