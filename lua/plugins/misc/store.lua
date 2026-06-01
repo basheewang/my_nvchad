81dcd4dc911351cfb53d4b5759937a832a56431f
Coeus <bashee.wang@gmail.com>
2026-06-01 12:06:46 +0800

    Add store.nvim plugin

---
+++ lua/plugins/misc/store.lua
@@ -0,0 +1,17 @@
+---@type NvPluginSpec
+---🤖 6200+ neovim plugins database with automatic installation! 🚀
+
+local M = {
+  "alex-popov-tech/store.nvim",
+  dependencies = {
+    { "OXY2DEV/markview.nvim", opts = {} },
+    -- Optional: inline image rendering in README previews (Kitty, Ghostty, WezTerm only)
+    -- { "3rd/image.nvim", opts = { integrations = { markdown = { enabled = false } } } },
+  },
+  opts = {
+    -- layout = "tab", -- recommended when using image preview
+  },
+  cmd = "Store",
+}
+
+return M
